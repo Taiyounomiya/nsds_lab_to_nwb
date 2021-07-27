f17db08048257fbfe03306c8baf96dc79ae7d06a
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-07-26 20:28:36 -0700

    check unused metadata items

--- nsds_lab_to_nwb/metadata/metadata_manager.py
+++ nsds_lab_to_nwb/metadata/metadata_manager.py
@@ -5,9 +5,10 @@ import itertools
 from collections import OrderedDict
 
 from nsds_lab_to_nwb.utils import (get_metadata_lib_path, get_stim_lib_path,
-                                   split_block_folder)
+                                   split_block_folder, str2bool)
 
 from nsds_lab_to_nwb.common.io import read_yaml, write_yaml
+from nsds_lab_to_nwb.common.time import LOCAL_TIMEZONE, get_date_string_only
 from nsds_lab_to_nwb.metadata.exp_note_reader import ExpNoteReader
 from nsds_lab_to_nwb.metadata.keymap_helper import apply_keymap
 from nsds_lab_to_nwb.metadata.resources import read_metadata_resource
@@ -31,7 +32,7 @@ class MetadataReader:
         self.block_metadata_path = block_metadata_path
         self.metadata_lib_path = get_metadata_lib_path(metadata_lib_path)
         self.block_folder = block_folder
-        _, self.animal_name, _ = split_block_folder(block_folder)
+        self.surgeon_initials, self.animal_name, self.block_tag = split_block_folder(block_folder)
         self.metadata_save_path = metadata_save_path
 
     def read(self):
@@ -42,6 +43,7 @@ class MetadataReader:
 
         self.parse()
         self.common_check()
+        self.complete_notes()
         self.extra_cleanup()
         if self.metadata_save_path is not None:
             write_yaml(f'{self.metadata_save_path}/{self.block_folder}_metadata_input_clean.yaml',
@@ -116,47 +118,69 @@ class MetadataReader:
                     'The signal is low pass filtered at 45 percent of the sample rate, '
                     'and high pass filtered at 2 Hz.')
 
+    def complete_notes(self):
+        # TODO: store notes for all blocks for this animal, not just this one?
+        self.__complete_surgery_note()
+
     def extra_cleanup(self):
         device_metadata = self.metadata_input['device']
         block_meta = self.metadata_input['block_meta']
+        experiment_meta = self.metadata_input['experiment_meta']
+
+        # validate and drop duplicate information
+        block_id = block_meta.pop('block_id', None)
+        block_tag_check = f'B{int(block_id):02d}'
+        if (block_id is not None) and (self.block_tag != block_tag_check):
+            raise ValueError(f'block id mismatch: {self.block_tag} vs. {block_tag_check}')
+
+        animal_number = experiment_meta.pop('animal_number', None)
+        animal_name_check = f'R{self.surgeon_initials}{int(animal_number):02d}'
+        if (animal_number is not None) and (self.animal_name != animal_name_check):
+            raise ValueError(f'animal number mismatch: {self.animal_name} vs. {animal_name_check}')
+        if len(experiment_meta) == 0:
+            self.metadata_input.pop('experiment_meta')
+
+        # read the clean_block switch
+        if 'is_clean_block' in block_meta:
+            block_meta['is_clean_block'] = str2bool(block_meta['is_clean_block'])
 
         # if a device was not actually used in this block, drop the corresponding metadata
         default_value = False
-        has_ecog = self.__convert_bool(block_meta.get('has_ecog', default_value))
-        has_poly = self.__convert_bool(block_meta.get('has_poly', default_value))
-        if not has_ecog:
+        block_meta['has_ecog'] = str2bool(block_meta.get('has_ecog', default_value))
+        block_meta['has_poly'] = str2bool(block_meta.get('has_poly', default_value))
+        if not block_meta['has_ecog']:
             device_metadata.pop('ECoG')
-        if not has_poly:
+        if not block_meta['has_poly']:
             device_metadata.pop('Poly')
             block_meta.pop('poly_ap_loc', None)
             block_meta.pop('poly_dev_loc', None)
 
-    def __convert_bool(self, s):
-        """Convert a True/False text (string) to a boolean value.
-        TODO: merge with the near-duplicate function utils.str2bool
+    def __complete_surgery_note(self):
+        experiment_meta = self.metadata_input['experiment_meta']
+        surgery = self.metadata_input['surgery']
 
-        Parameters
-        ---------
-        s : bool, str or int
-            Something equivalent to True or False.
+        if ('procedure_date' in experiment_meta):
+            surgery_date = experiment_meta.pop('procedure_date')
+            try:
+                surgery_date = get_date_string_only(surgery_date)
+            except ValueError:
+                # datetime format error: just use input string as-is
+                pass
+            surgery += f'\nSurgery date: {surgery_date}'
 
-        Returns
-        -------
-        A corresponding boolean variable.
-        """
-        if isinstance(s, bool):
-            return s
-        if isinstance(s, int):
-            return bool(s)
-        if not isinstance(s, str):
-            raise TypeError('Allowed types are bool, int or string.')
+        if ('procedure_time' in experiment_meta):
+            surgery_time = experiment_meta.pop('procedure_time')
+            surgery += f'\nSurgery time: {surgery_time} ({LOCAL_TIMEZONE})'
 
-        # simply search from a word pool
-        if s.lower() in ['true', '1', 't', 'y', 'yes']:
-            return True
-        if s.lower() in ['false', '0', 'f', 'n', 'no']:
-            return False
-        raise ValueError('Cannot convert \'{}\' to boolean.'.format(s))
+        if 'surgery_notes' in experiment_meta:
+            surgery_notes = experiment_meta.pop('surgery_notes')
+            surgery += f'\nSurgery notes: {surgery_notes}'
+
+        if 'surgery_outcome' in experiment_meta:
+            surgery_outcome = experiment_meta.pop('surgery_outcome')
+            surgery += f'\nSurgery outcome: {surgery_outcome}'
+
+        self.metadata_input['surgery'] = surgery
 
 
 class LegacyMetadataReader(MetadataReader):
@@ -194,6 +218,10 @@ class LegacyMetadataReader(MetadataReader):
         self.metadata_input = apply_keymap(self.metadata_input.copy(),
                                            keymap_file='metadata_keymap_legacy')
 
+    def complete_notes(self):
+        # TODO: read and store old experiment notes
+        pass
+
     def extra_cleanup(self):
         # fill in old subject information
         old_subject_input = read_metadata_resource('old_subject_metadata')
@@ -204,10 +232,38 @@ class LegacyMetadataReader(MetadataReader):
                 self.metadata_input['subject'][key] = old_subject_metadata[key]
 
         # put bad_chs to right places
-        bad_chs_dict = self.metadata_input['device'].pop('bad_chs', None)
+        device_metadata = self.metadata_input['device']
+        bad_chs_dict = device_metadata.pop('bad_chs', None)
         if bad_chs_dict is not None:
             for dev_name, bad_chs in bad_chs_dict.items():
-                self.metadata_input['device'][dev_name]['bad_chs'] = bad_chs
+                device_metadata[dev_name]['bad_chs'] = bad_chs
+
+        # data acquisition system
+        if 'dac' in self.metadata_input:
+            acq = self.metadata_input.pop('dac')
+            for dev_name in ('ECoG', 'Poly'):
+                if dev_name in device_metadata:
+                    device_metadata[dev_name]['acq'] = acq
+
+        # check and drop extra device information
+        if 'comment' in device_metadata:
+            dev_comment = device_metadata.pop('comment', '').strip(' ')
+            # usually empty, but keep if there is content
+            if len(dev_comment) > 0:
+                for dev_name in ('ECoG', 'Poly'):
+                    if dev_name in device_metadata:
+                        device_metadata[dev_name]['comment'] = dev_comment
+
+        if 'htk_meta' in self.metadata_input:
+            # assuming that these are fixed (so no need to keep)
+            _mark = 'mrk11.htk'
+            _audio = 'aud11.htk'
+            if self.metadata_input['htk_meta'].pop('mark', _mark) != _mark:
+                raise ValueError('Unexpected filename for HTK mark. Perhaps we should keep this?')
+            if self.metadata_input['htk_meta'].pop('audio', _audio) != _audio:
+                raise ValueError('Unexpected filename for HTK audio. Perhaps we should keep this?')
+            if len(self.metadata_input['htk_meta']) == 0:
+                self.metadata_input.pop('htk_meta')
 
         # final touches...
         if self.experiment_type == 'auditory':
@@ -387,13 +443,15 @@ class MetadataManager:
                 # set up device descriptions;
                 # prepare two versions for device and e-group
                 basic_description = f"{dev_conf.pop('nchannels')}-ch {key}"
-                # for new data only
                 extra_device_description = ""
                 if 'serial' in dev_conf:
+                    # for new data only
                     extra_device_description += f"serial={dev_conf.pop('serial')}. "
                 if 'acq' in dev_conf:
-                    extra_device_description += (
-                        f"acq={dev_conf.pop('acq').replace(' ', '-')}. ")
+                    acquisition = dev_conf.pop('acq').replace(' ', '-')
+                    extra_device_description += f'acq={acquisition}. '
+                if 'comment' in dev_conf:
+                    extra_device_description += f"{dev_conf.pop('comment')}. "
 
                 # keep poly_neighbors, if applicable, after channel remapping
                 poly_neighbors = dev_conf.pop('poly_neighbors', None)
@@ -407,16 +465,19 @@ class MetadataManager:
                     location_details = ''
 
                 dev_conf['descriptions'] = {} # ignore existing placeholder text
-                dev_conf['descriptions']['device_description'] = (
+                device_description = (
                     f"{basic_description} from {dev_conf['manufacturer']} "
                     f"({dev_conf.pop('device_type')}). "
                     f"{extra_device_description}"
                     f"n_columns={dev_conf.pop('n_columns')}, "
                     f"n_rows={dev_conf.pop('n_rows')}, "
-                    f"orientation={dev_conf.pop('orientation')}, "
-                    f"xspacing={dev_conf.pop('xspacing', '(unknown)')}mm, "
-                    f"yspacing={dev_conf.pop('yspacing', '(unknown)')}mm, "
-                    f"prefix={dev_conf['prefix']}.")
+                    f"orientation={dev_conf.pop('orientation')}, ")
+                for sp in ('xspacing', 'yspacing', 'zspacing'):
+                    if sp in dev_conf:
+                        device_description += f"{sp}={dev_conf.pop(sp)}mm, "
+                device_description += f"prefix={dev_conf['prefix']}."
+                dev_conf['descriptions']['device_description'] = device_description
+
                 dev_conf['descriptions']['electrode_group_description'] = (
                     f"{basic_description}. "
                     f"{location_details}").strip()
@@ -426,3 +487,7 @@ class MetadataManager:
                         len(device_metadata[key]['location']) == 0):
                     if self.experiment_type == 'auditory':
                         device_metadata[key]['location'] = 'AUD'
+
+                # drop unused items
+                if 'probe_config' in dev_conf:
+                    dev_conf.pop('probe_config')
