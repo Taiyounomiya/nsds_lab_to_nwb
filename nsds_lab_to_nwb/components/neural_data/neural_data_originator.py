f9169f3bbc102f983c8a16ef1b0679ef93b2db7c
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-09-01 15:38:24 -0700

    minor code rearrangement

--- nsds_lab_to_nwb/components/neural_data/neural_data_originator.py
+++ nsds_lab_to_nwb/components/neural_data/neural_data_originator.py
@@ -24,47 +24,6 @@ class NeuralDataOriginator():
             logger.info('Using TDT')
             self.neural_data_reader = TDTReader(self.dataset.tdt_path)
 
-    def make_description(self, device_name):
-        description = self.metadata['experiment_description']
-        description += '. Recordings from {0:s} sampled at {1:f} Hz.'.format(device_name,
-                                                                             self.hardware_rate)
-        if self.resample_flag:
-            description += ' Then resampled down to {0:d} Hz'.format(int(self.resample_rate))
-        return description
-
-    def make_comments(self, dev_conf):
-        if 'bad_chs' not in dev_conf:
-            return ''
-
-        ch_map = dev_conf['ch_map']
-        comments = 'bad_channels=['
-        comments += (", ".join([str(ch_map[i]['electrode_id'])  # re-map channels
-                                for i in dev_conf['bad_chs']])).rstrip(', ')
-        comments += "]"
-        return comments
-
-    def resample(self, data):
-        # only resample if rate is not at nearest kHz
-        rate = self.hardware_rate
-        if (rate / 1000 % 1) > 0:
-            new_freq = (rate // 1000) * 1000
-            logger.info(f' - resampling from {rate} Hz to {new_freq} Hz')
-            new_data = resample(data, new_freq, rate)
-            self.resample_rate = new_freq
-            return new_data
-        else:
-            # no need to resample, already at nearest kHz
-            logger.info(f' - already at integer kHz; no need to resample')
-            self.resample_flag = False
-            return data
-
-    def _get_rate(self):
-        if self.resample_rate is None:
-            rate = self.hardware_rate
-        else:
-            rate = self.resample_rate
-        return rate * 1.0
-
     def make(self, nwb_content, electrode_table_regions):
         for device_name, dev_conf in self.metadata['device'].items():
             if isinstance(dev_conf, str): # skip other annotations
@@ -83,10 +42,10 @@ class NeuralDataOriginator():
                     logger.info('Resampling successful.')
 
                 # make description
-                description = self.make_description(device_name)
+                description = self._get_description(device_name)
 
                 # make comments (mention bad channels)
-                comments = self.make_comments(dev_conf)
+                comments = self._get_comments(dev_conf)
 
                 electrode_table_region = electrode_table_regions[device_name]
                 e_series = ElectricalSeries(name=device_name,
@@ -101,3 +60,44 @@ class NeuralDataOriginator():
                 logger.debug(f' - Description: {description}')
                 logger.debug(f' - Comments: {comments}')
                 nwb_content.add_acquisition(e_series)
+
+    def resample(self, data):
+        # only resample if rate is not at nearest kHz
+        rate = self.hardware_rate
+        if (rate / 1000 % 1) > 0:
+            new_freq = (rate // 1000) * 1000
+            logger.info(f' - resampling from {rate} Hz to {new_freq} Hz')
+            new_data = resample(data, new_freq, rate)
+            self.resample_rate = new_freq
+            return new_data
+        else:
+            # no need to resample, already at nearest kHz
+            logger.info(f' - already at integer kHz; no need to resample')
+            self.resample_flag = False
+            return data
+
+    def _get_description(self, device_name):
+        description = self.metadata['experiment_description']
+        description += '. Recordings from {0:s} sampled at {1:f} Hz.'.format(device_name,
+                                                                             self.hardware_rate)
+        if self.resample_flag:
+            description += ' Then resampled down to {0:d} Hz'.format(int(self.resample_rate))
+        return description
+
+    def _get_comments(self, dev_conf):
+        if 'bad_chs' not in dev_conf:
+            return ''
+
+        ch_map = dev_conf['ch_map']
+        comments = 'bad_channels=['
+        comments += (", ".join([str(ch_map[i]['electrode_id'])  # re-map channels
+                                for i in dev_conf['bad_chs']])).rstrip(', ')
+        comments += "]"
+        return comments
+
+    def _get_rate(self):
+        if self.resample_rate is None:
+            rate = self.hardware_rate
+        else:
+            rate = self.resample_rate
+        return rate * 1.0
